28a99affcf28d28de4e8dabe4a35e9f2c183ccc8
TristanSoenen <soenentristan@gmail.com>
2024-10-30 14:43:25 +0100

    added 2 colors for the bullets and added collision to the bullets

--- SpaceWar/Source/SpaceWar/Bullet.cpp
+++ SpaceWar/Source/SpaceWar/Bullet.cpp
@@ -3,22 +3,44 @@
 
 #include "SpaceWar/Bullet.h"
 
-#include "MeshAttributes.h"
+#include "EnemyBoss.h"
+#include "SpaceWar/PlayerSpaceCraft.h"
 
 // Sets default values
 ABullet::ABullet()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void ABullet::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	GetWorld()->GetTimerManager().SetTimer(m_KillTimer, this, &ABullet::KillBullet, m_KillDelay, false);
+	m_Mesh = FindComponentByClass<UStaticMeshComponent>();
+	if(m_Mesh)
+		m_Mesh->OnComponentBeginOverlap.AddDynamic(this, &ABullet::OnOverlapStart);
 }
+
+void ABullet::InitializeBullet(bool playerFired, float speed, const FVector &direction)
+{
+	m_FiredByPlayer = playerFired;
+	m_Direction = direction;
+	m_Speed = speed;
+}
+
+void ABullet::SetBulletMaterial()
+{
+	if(m_Mesh == nullptr)
+		return;
+	
+	if(m_FiredByPlayer == true)
+		m_Mesh->SetMaterial(0, m_BulletMaterialPlayer);
+	else
+		m_Mesh->SetMaterial(0, m_BulletMaterialEnemy);
+}
+
 
 // Called every frame
 void ABullet::Tick(float DeltaTime)
@@ -29,3 +51,54 @@ void ABullet::Tick(float DeltaTime)
 	SetActorLocation(nextPosition);
 }
 
+void ABullet::KillBullet()
+{
+	Destroy();
+}
+
+void ABullet::OnOverlapStart(UPrimitiveComponent *OverlappedComponent, AActor *OtherActor,
+	UPrimitiveComponent *OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult &SweepResult)
+{
+	//check if player is hit
+	CheckPlayerHit(OtherActor);
+
+	//Enemy Hit
+	CheckEnemyHit(OtherActor);
+
+	//Check Collision Between Bullets
+	CheckBulletHit(OtherActor);
+}
+
+
+void ABullet::CheckPlayerHit(AActor* other)
+{
+	//Returns if the player is null or the bullet is fired by the player
+	APlayerSpaceCraft* player = Cast<APlayerSpaceCraft>(other);
+	if (player == nullptr || m_FiredByPlayer == true)
+		return;
+	
+	player->ReduceHealth(m_DamageToPlayer);
+	KillBullet();
+}
+
+void ABullet::CheckBulletHit(AActor *other)
+{
+	//Destroy both bullets if they hit each other
+	ABullet* otherBullet = Cast<ABullet>(other);
+	if (otherBullet == nullptr)
+		return;
+	
+	otherBullet->KillBullet();
+	KillBullet();
+}
+
+void ABullet::CheckEnemyHit(AActor *other)
+{
+	//Returns if the player is null or the bullet is fired by the player
+	AEnemyBoss* enemy = Cast<AEnemyBoss>(other);
+	if (enemy == nullptr || m_FiredByPlayer == false)
+		return;
+
+	enemy->KillEnemy();
+	KillBullet();
+}
--- SpaceWar/Source/SpaceWar/Bullet.h
+++ SpaceWar/Source/SpaceWar/Bullet.h
@@ -22,9 +22,34 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	void SetDirection(FVector Direction) { m_Direction = Direction; }
+	void SetKillDelay(float delay) { m_KillDelay = delay; }
+	void InitializeBullet(bool playerFired, float speed, const FVector& direction);
+	
+	UFUNCTION()
+	void OnOverlapStart(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	void SetBulletMaterial();
 private:
+	//functions 
+	void CheckPlayerHit(AActor* other);
+	void CheckBulletHit(AActor* other);
+	void CheckEnemyHit(AActor* other);
+	
+	//Bullet params
 	UPROPERTY(EditAnywhere)
-	float m_Speed;
+	float m_Speed{};
+	UPROPERTY(EditAnywhere)
+	UMaterialInstance* m_BulletMaterialPlayer;
+	UPROPERTY(EditAnywhere)
+	UMaterial* m_BulletMaterialEnemy;
 	FVector m_Direction{};
+	int m_DamageToPlayer = 25;
+	bool m_FiredByPlayer;
+	UPROPERTY()
+	UStaticMeshComponent* m_Mesh;
+	
+	//Timer
+	FTimerHandle m_KillTimer;
+	void KillBullet();
+	float m_KillDelay = 5.0f;
 };
